8af4966ece481d29e8aae22a3705960cadbc3ad4
Jin <hojin.yoo@outlook.com>
2013-06-07 16:16:19 -0400

    added a few restaurants

--- lunchbot.rb
+++ lunchbot.rb
@@ -5,10 +5,13 @@ class Lunchbot
 	Restaurant = Struct.new(:name, :description, :location)
 
 	def initialize
-		mcDonalds = Restaurant.new("McDonalds", "Cheap burgers", "200 Front St. West")
-		subway = Restaurant.new("Subway", "Good subs!", "2 287 King W")
-		burritoBoys = Restaurant.new("Burrito Boys", "Awesome burritos", "218 Adelaide St West")
-		@restaurants = [] << mcDonalds << subway << burritoBoys
+		mcDonalds = Restaurant.new("McDonalds", "McDickerson, Cheap burgers.", "200 Front St W")
+		subway = Restaurant.new("Subway", "Good subs!", "2 287 King St W")
+		burritoBoyz = Restaurant.new("Burrito Boyz", "Serve awesome burritos, but really busy.", "218 Adelaide St W")
+		smokespoutinerie = Restaurant.new("Smoke's Poutinerie", "You can find all sorts of poutine!", "218 Adelaide St W")
+		banhmiboys = Restaurant.new("Banh Mi Boys", "Has amazing fusion Asian sandwiches.", "392 Queen St W")
+		village = Restaurant.new("Village by the Grange", "Food court with cheap, but decent food", "53 McCaul St")
+		@restaurants = [] << mcDonalds << subway << burritoBoyz <<smokespoutinerie << banhmiboys << village
 		@group = []
 	end
 
